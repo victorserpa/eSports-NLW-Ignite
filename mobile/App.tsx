3f5bb5964db054eb8fd5ae5d6d3a3f75fba24566
Victor Schmucker Serpa <schmvitoor@gmail.com>
2022-09-17 00:04:28 -0300

    First commit
    Commit completo do NLW eSports Ignite

---
+++ mobile/App.tsx
@@ -0,0 +1,66 @@
+import { useRef, useEffect } from "react";
+
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
+import { Subscription } from "expo-modules-core";
+import * as Notifications from 'expo-notifications';
+
+import { Routes } from "./src/routes";
+import { Background } from "./src/components/Background";
+import { Loading } from "./src/components/Loading";
+
+import "./src/service/notificationConfig";
+import { getPushNotificationToken } from "./src/service/getPushNotificationToken";
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  });
+  
+  useEffect(() => {
+    getNotificationListener.current = 
+    Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification)
+    })
+    
+    responseNotificationListener.current = 
+    Notifications.addNotificationReceivedListener(response => {
+      console.log(response.date);
+    })
+    
+    return () => {
+      if (getNotificationListener.current && responseNotificationListener.current){
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    }
+  }, []);
+  
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Routes /> : <Loading />}
+    </Background>
+  );
+}
